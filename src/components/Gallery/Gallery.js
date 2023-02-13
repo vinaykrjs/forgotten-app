7f410fdf96ca86283c5a27c17b91031e3abc5067
Vinay kumar Gujja <vinay.gujja@gspann.com>
2023-02-13 17:37:26 +0530

    folder added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { sculptureList } from '../../data/data';
+import { sculptureList } from '../../../public/data/data';
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
